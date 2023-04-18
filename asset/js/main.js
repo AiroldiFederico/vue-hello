b1eaa90e48e75998bc5c9195ae59a76862df955e
Federico Airoldi <airoldibusiness@gmail.com>
2023-04-18 22:28:09 +0200

    FASE 2 - bonus e ritocchi

--- asset/js/main.js
+++ asset/js/main.js
@@ -13,6 +13,9 @@ createApp({
       
       titoloH1: 'Vue Js',
 
+      imgsrc: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/1200px-Vue.js_Logo_2.svg.png',
+
+      classimg: 'w-25',
     }
   },
 
